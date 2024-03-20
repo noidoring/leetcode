c4ba416ddf91f74f473b0c9c9eb597934c25fcce
Rodion <noidoring@gmail.com>
2024-03-20 20:17:42 +0300

    start binary search

--- list/search_insert_position.py
+++ list/search_insert_position.py
@@ -4,3 +4,20 @@ def searchInsert(nums, target):
     lst.sort()
     return lst.index(target)
     
+
+def searchInsertV2(nums, target):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = nums[mid]
+
+        if guess <= target:
+            return mid - 1
+        
+
+nums = [1, 2, 3, 6, 7, 9, 10]
+target = 5
+
+print(searchInsertV2(nums, target))
